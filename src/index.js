c8b18e1b7acc9183de3a505dd8838f5033e6a3a9
SF Knute <s.fedotov@knute.edu.ua>
2025-11-05 08:39:42 +0200

    saved

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 console.log(2);
 
 import express from 'express'
+import { products } from './products.js';
 const app = express()
-const port = 3000
+const port = 8080
+
+const API_KEY=process.env.lecture5313;
 
 app.get('/', (req, res) => {
   res.send('Hello seerhii')
+})
+
+
+app.get('/products',(req,res)=>{
+    if(req.header('authorization') == API_KEY){
+        res.json(products);
+    }else{
+        res.status(401).send('no auth');
+    }   
 })
 
 app.listen(port, () => {
---
+++ src/products.js
@@ -0,0 +1,12 @@
+export let products = [
+    {
+        name:'iPhone',
+        price:999,
+        count: 124
+    },
+    {
+        name:"Samsung",
+        price: 799,
+        count: 334
+    }
+]
